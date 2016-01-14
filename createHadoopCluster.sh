e572a04b769b56a5eaa1ac38567caa2b514604bf
SPIRALS <spirals@193-51-236-73.lille.inria.fr>
2016-01-14 19:42:23 +0100

    rm start-hadoop

--- createHadoopCluster.sh
+++ createHadoopCluster.sh
@@ -11,6 +11,7 @@ docker-machine create --driver virtualbox hadoop-control
 docker-machine create --driver virtualbox hadoop-hibench
 docker-machine create --driver virtualbox hadoop-compute1
 
+
 eval $(docker-machine env hadoop-control)
 docker pull zhangbbo/hadoop-benchmark:hadoop-controlnode-multinodes
 docker-compose --x-networking -f hadoopControlNode.yml up -d
@@ -23,18 +24,18 @@ eval $(docker-machine env hadoop-compute1)
 docker pull zhangbbo/hadoop-benchmark:hadoop-computenode-multinodes
 docker-compose --x-networking -f hadoopComputeNode.yml up -d
 
+
 echo "Collect Hostname IP maping."
 echo "$(docker-machine ip hadoop-control) hadoop-control" >> /Users/spirals/data/hosts
 echo "$(docker-machine ip hadoop-hibench) hadoop-hibench" >> /Users/spirals/data/hosts
 echo "$(docker-machine ip hadoop-compute1) $(docker ps -q -l)" >> /Users/spirals/data/hosts
+
 
 echo "Distribute Hostname IP maping."
 docker $(docker-machine config hadoop-control) exec hadoop-control /bin/bash -c "sh /getMaping.sh"
 docker $(docker-machine config hadoop-hibench) exec hadoop-hibench /bin/bash -c "sh /getMaping.sh"
 docker $(docker-machine config hadoop-compute1) exec $(docker $(docker-machine config hadoop-compute1) ps -q -l) /bin/bash -c "sh /getMaping.sh"
 
-echo"Start Hadoop"
-docker $(docker-machine config hadoop-control) exec hadoop-control /bin/bash -c "sh /start-hadoop.sh"
 
 eval $(docker-machine env hadoop-hibench)
 echo "In Hadoop HiBench Node now!"
--- hadoop-control/entrypoint.sh
+++ hadoop-control/entrypoint.sh
@@ -4,4 +4,12 @@ sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/co
 sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/mapred-site.xml
 sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/yarn-site.xml 
 
+/root/hadoop/bin/hadoop namenode -format
+
+/root/hadoop/sbin/hadoop-daemon.sh start namenode
+
+/root/hadoop/sbin/hadoop-daemon.sh start secondarynamenode
+
+/root/hadoop/sbin/yarn-daemon.sh start resourcemanager
+
 
