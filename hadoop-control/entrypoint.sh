6986ee861036d5d2b8d988d733d2b99df5dff141
SPIRALS <spirals@193-51-236-73.lille.inria.fr>
2016-01-14 20:22:10 +0100

    controlnode local IP

--- hadoop-control/entrypoint.sh
+++ hadoop-control/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/core-site.xml
-sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/mapred-site.xml
-sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/yarn-site.xml 
-
 /root/hadoop/bin/hadoop namenode -format
 
 /root/hadoop/sbin/hadoop-daemon.sh start namenode
