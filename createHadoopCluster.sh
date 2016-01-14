e6fa6a7e854b99eec2e2772927f96310c87feb32
SPIRALS <spirals@193-51-236-73.lille.inria.fr>
2016-01-14 13:02:00 +0100

    get hostname

--- createHadoopCluster.sh
+++ createHadoopCluster.sh
@@ -8,11 +8,8 @@
 mkdir /Users/spirals/data
 
 docker-machine create --driver virtualbox hadoop-control
-echo "$(docker-machine ip hadoop-control) hadoop-control" >> /Users/spirals/data/hosts
 docker-machine create --driver virtualbox hadoop-hibench
-echo "$(docker-machine ip hadoop-hibench) hadoop-control" >> /Users/spirals/data/hosts
 docker-machine create --driver virtualbox hadoop-compute1
-echo "$(docker-machine ip hadoop-compute1) hadoop-control" >> /Users/spirals/data/hosts
 
 eval $(docker-machine env hadoop-control)
 docker-compose --x-networking -f hadoopControlNode.yml up -d
@@ -20,6 +17,14 @@ eval $(docker-machine env hadoop-hibench)
 docker-compose --x-networking -f hadoopHibenchNode.yml up -d
 eval $(docker-machine env hadoop-compute1)
 docker-compose --x-networking -f hadoopComputeNode.yml up -d
+
+echo "$(docker-machine ip hadoop-control) hadoop-control" >> /Users/spirals/data/hosts
+echo "$(docker-machine ip hadoop-hibench) hadoop-hibench" >> /Users/spirals/data/hosts
+echo "$(docker-machine ip hadoop-compute1) $(docker ps -q -l)" >> /Users/spirals/data/hosts
+
+docker $(docker-machine config hadoop-control) exec hadoop-control /bin/bash -c "sh /getMaping.sh"
+docker $(docker-machine config hadoop-hibench) exec hadoop-hibench /bin/bash -c "sh /getMaping.sh"
+docker $(docker-machine config hadoop-compute1) exec $(docker ps -q -l) /bin/bash -c "sh /getMaping.sh"
 
 eval $(docker-machine env hadoop-hibench)
 echo "In Hadoop HiBench Node now!"
--- hadoop-compute/entrypoint.sh
+++ hadoop-compute/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-/bin/bash -c /getMaping.sh
-
 /root/hadoop/sbin/hadoop-daemon.sh start datanode
 
 /root/hadoop/sbin/yarn-daemon.sh start nodemanager 
