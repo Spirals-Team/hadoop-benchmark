524961f0d290d49b530885e50e41ef39c4e4a107
SPIRALS <spirals@193-51-236-73.lille.inria.fr>
2016-01-14 16:37:40 +0100

    control container name

--- createHadoopCluster.sh
+++ createHadoopCluster.sh
@@ -12,18 +12,25 @@ docker-machine create --driver virtualbox hadoop-hibench
 docker-machine create --driver virtualbox hadoop-compute1
 
 eval $(docker-machine env hadoop-control)
+docker pull zhangbbo/hadoop-benchmark:hadoop-controlnode-multinodes
 docker-compose --x-networking -f hadoopControlNode.yml up -d
+
 eval $(docker-machine env hadoop-hibench)
+docker pull zhangbbo/hadoop-benchmark:hadoop-hibenchnode-multinodes
 docker-compose --x-networking -f hadoopHibenchNode.yml up -d
+
 eval $(docker-machine env hadoop-compute1)
+docker pull zhangbbo/hadoop-benchmark:hadoop-computenode-multinodes
 docker-compose --x-networking -f hadoopComputeNode.yml up -d
 
-echo "$(docker-machine ip hadoop-control) hadoop-control" >> /Users/spirals/data/hosts
-echo "$(docker-machine ip hadoop-hibench) hadoop-hibench" >> /Users/spirals/data/hosts
+echo "Collect Hostname IP maping."
+echo "$(docker-machine ip hadoop-control) hadoop-controlnode" >> /Users/spirals/data/hosts
+echo "$(docker-machine ip hadoop-hibench) hadoop-hibenchnode" >> /Users/spirals/data/hosts
 echo "$(docker-machine ip hadoop-compute1) $(docker ps -q -l)" >> /Users/spirals/data/hosts
 
-docker $(docker-machine config hadoop-control) exec hadoop-control /bin/bash -c "sh /getMaping.sh"
-docker $(docker-machine config hadoop-hibench) exec hadoop-hibench /bin/bash -c "sh /getMaping.sh"
+echo "Distribute Hostname IP maping."
+docker $(docker-machine config hadoop-control) exec hadoop-controlnode /bin/bash -c "sh /getMaping.sh"
+docker $(docker-machine config hadoop-hibench) exec hadoop-hibenchnode /bin/bash -c "sh /getMaping.sh"
 docker $(docker-machine config hadoop-compute1) exec $(docker $(docker-machine config hadoop-compute1) ps -q -l) /bin/bash -c "sh /getMaping.sh"
 
 eval $(docker-machine env hadoop-hibench)
--- hadoop-control/entrypoint.sh
+++ hadoop-control/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/core-site.xml
-sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/mapred-site.xml
-sed -i -- "s/ControlNodeHostName/$(echo $HOSTNAME)/g" /root/hadoop/etc/hadoop/yarn-site.xml 
+sed -i -- "s/ControlNodeHostName/hadoop-control/g" /root/hadoop/etc/hadoop/core-site.xml
+sed -i -- "s/ControlNodeHostName/hadoop-control/g" /root/hadoop/etc/hadoop/mapred-site.xml
+sed -i -- "s/ControlNodeHostName/hadoop-control/g" /root/hadoop/etc/hadoop/yarn-site.xml 
 
 /root/hadoop/bin/hadoop namenode -format
 
