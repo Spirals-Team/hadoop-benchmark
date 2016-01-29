496cffde35cc43b7de23c5d26674e3e41424b9d2
SPIRALS <spirals@193-51-236-73.lille.inria.fr>
2016-01-29 17:30:38 +0100

    hibench conf

--- benchmarks/hibench/run.sh
+++ benchmarks/hibench/run.sh
@@ -3,8 +3,8 @@ set -e
 
 ALL_BENCHMARKS="wordcount sort terasort sleep"
 
-base=$(dirname "$(cd "$(dirname "$0")"; pwd)/$(basename "$0")")
-docker build -t hadoop-benchmark-hibench "$base/image"
+#base=$(dirname "$(cd "$(dirname "$0")"; pwd)/$(basename "$0")")
+#docker build -t hadoop-benchmark-hibench "$base/image"
 
 if [[ $# -lt 1 ]]; then
   BENCHMARKS=$ALL_BENCHMARKS
@@ -18,5 +18,5 @@ docker run \
   --net hadoop-net \
   --name hadoop-benchmark-hibench \
   -h hadoop-benchmark-hibench \
-  hadoop-benchmark-hibench \
+  spirals/hadoop-benchmark:hadoop-benchmark-hibench \
   --benchmarks "$BENCHMARKS"
