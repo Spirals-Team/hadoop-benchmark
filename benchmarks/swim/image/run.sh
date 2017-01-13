8d4c5ade779e370296c477b7349bf41de6e1c146
Filip Krikava <krikava@gmail.com>
2017-01-13 00:26:47 -0500

    Upload the results to HDFS

--- benchmarks/swim/image/run.sh
+++ benchmarks/swim/image/run.sh
@@ -2,12 +2,16 @@
 set -e
 
 echo "Preparing SWIM:"
+[[ -d workGenInput ]] && rm -fr workGenInput
+hdfs dfs -rm -r -f /user/root/workGenInput
 hadoop jar $SWIM_HOME/HDFSWrite.jar org.apache.hadoop.examples.HDFSWrite -conf $SWIM_HOME/randomwriter_conf.xsl workGenInput
 
 echo "Running SWIM:"
 $SWIM_HOME/run-jobs-all.sh
 
-/bin/bash
+logs="workGenLogs-$(date +"%Y%m%d-%H%M").tgz"
+tar cfvz "$logs" "$SWIM_HOME/workGenLogs"
+hdfs dfs -put "$logs" "/user/root/$logs"
 
-
-
+echo "Benchmarks finished"
+echo "Logs uploaded to HDFS: /user/root/$logs"
