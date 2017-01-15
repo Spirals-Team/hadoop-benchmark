a8b4965eb9f14246aa0b081b2ed9b673ab4fb4b2
Filip Krikava <krikava@gmail.com>
2017-01-15 13:01:38 -0500

    Fixed uploading hibench.report

--- benchmarks/hibench/image/run.sh
+++ benchmarks/hibench/image/run.sh
@@ -8,20 +8,20 @@ case "$1" in
     echo $@ | tr ' ' '\n' > $HIBENCH_HOME/conf/benchmarks.lst
     echo "Running benchmarks:"
     cat $HIBENCH_HOME/conf/benchmarks.lst
-
-    for i in $(seq 1 10);
-    do
-        $HIBENCH_HOME/bin/run-all.sh
-        cat $HIBENCH_HOME/hibench.report
-
-        hdfs dfs -copyFromLocal $HIBENCH_HOME/hibench.report hdfs:///report/hibench-$(date +"%s").report
-    done
+    $HIBENCH_HOME/bin/run-all.sh
   ;;
   *)
     exec "$@"
   ;;
 esac
 
+report=$HIBENCH_HOME/hibench.report
+dest="/user/root/hibench-$(date +"%Y%m%d-%H%M").report"
+hdfs dfs -put "$report" "$dest"
+
 echo "Benchmarks finished"
-echo "All hibench.report files are uploaded to HDFS (in hdfs:///)"
-echo "To download these reports to current path, run './cluster.sh hdfs-download hdfs:///report/ .'"
+echo
+cat "$report"
+echo
+echo "The report has been uploaded to HDFS: $dest"
+echo "To download, run ./cluster.sh hdfs-download \"$dest\""
