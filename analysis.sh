57a480a9e431e846179d3b296d67ec1abad51550
Filip Krikava <krikava@gmail.com>
2017-01-16 17:55:21 -0500

    Removed the analysis, it is not in the `benchmarks/swim/analysis`

--- analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {0..49}; do cat workGenLogs-Static/job-$i.txt | grep "The job took" | awk -v var=$i '{print var " "  $4}' >> all-jobs-duration-static.txt; done
-
-for i in {0..49}; do cat workGenLogs-Dynamic/job-$i.txt | grep "The job took" | awk -v var=$i '{print var " "  $4}' >> all-jobs-duration-dynamic.txt; done
-
-Rscript analysis.R
-
-pdfjam compare.pdf 2 -o compare.pdf
